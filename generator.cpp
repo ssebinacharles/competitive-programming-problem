5a86dd776cdb7e093456e2d8e7937012dbed8501
ssebinacharles <ssebinacharles50@gmail.com>
2026-08-28 10:35:19 +0000

    update

---
+++ generator.cpp
@@ -0,0 +1,30 @@
+// generator.cpp
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char** argv) {
+    // usage: generator <seed> <maxN> <maxAbsA> <maxAbsK>
+    long long seed = argc > 1 ? atoll(argv[1]) : 1;
+    int maxN = argc > 2 ? atoi(argv[2]) : 10;
+    long long maxAbsA = argc > 3 ? atoll(argv[3]) : 10;
+    long long maxAbsK = argc > 4 ? atoll(argv[4]) : 10;
+
+    mt19937_64 rng(seed);
+    auto randRange = [&](long long lo, long long hi) {
+        uniform_int_distribution<long long> dist(lo, hi);
+        return dist(rng);
+    };
+
+    int t = 1;
+    printf("%d\n", t);
+    int n = (int)randRange(1, maxN);
+    long long K = randRange(-maxAbsK, maxAbsK);
+    int Lmin = (int)randRange(1, n);
+    int Lmax = (int)randRange(Lmin, n);
+    printf("%d %lld %d %d\n", n, K, Lmin, Lmax);
+    for (int i = 0; i < n; i++) {
+        printf("%lld ", randRange(-maxAbsA, maxAbsA));
+    }
+    printf("\n");
+    return 0;
+}
---
+++ solution_bf.cpp
@@ -0,0 +1,31 @@
+// solution_bf.cpp
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    while (t--) {
+        int n;
+        ll K;
+        int Lmin, Lmax;
+        scanf("%d %lld %d %d", &n, &K, &Lmin, &Lmax);
+        vector<ll> a(n + 1);
+        for (int i = 1; i <= n; i++) scanf("%lld", &a[i]);
+
+        ll answer = 0;
+        for (int l = 1; l <= n; l++) {
+            ll sum = 0;
+            for (int r = l; r <= n; r++) {
+                sum += a[r];
+                int len = r - l + 1;
+                if (len < Lmin) continue;
+                if (len > Lmax) break;
+                if (sum >= K * (ll)len) answer++;
+            }
+        }
+        printf("%lld\n", answer);
+    }
+    return 0;
+}
